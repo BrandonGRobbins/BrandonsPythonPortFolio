aaf870a77c259d0311923179b6cc7a9123156187
Brandon Robbins <42588183+BrandonGRobbins@users.noreply.github.com>
2018-10-25 21:45:15 -0600

    Add files via upload

---
+++ BirbChecker.py
@@ -0,0 +1,8 @@
+def bird_available():
+        bird=input("enter a bird")
+        bird_types = "crow", "robin", "parrot", "eagle", "sandpiper", "hawk", "piegon"
+        if bird in bird_types:
+                print("yes")
+        else:
+                print("No")
+bird_available()
